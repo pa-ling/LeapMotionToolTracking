21ddb407357db3ef55d2d8ba28d4d230bed459c7
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-01-04 14:53:19 +0100

    Remove Leap.dll from DLL and instead get Leap Images from Unity side

--- TestDLL/TestDLL/TestDLLSort.cpp
+++ TestDLL/TestDLL/TestDLLSort.cpp
@@ -36,35 +36,35 @@ extern "C" {
 
 	/* This function gets image data and processes it*/
 	void __declspec(dllexport) ProcessImageData(unsigned char* in, unsigned char* out, int width, int height) {
-		Mat frame(height, width, CV_8UC4, in);
+		Mat frame(height, width, CV_8UC1, in);
 
 		// Process frame here …
 		//cvtColor(frame, frame, COLOR_BGR2GRAY);
-		cvtColor(frame, frame, COLOR_BGR2BGRA);
-		memcpy(out, frame.data, frame.total() * frame.elemSize());
+		//cvtColor(frame, frame, COLOR_BGR2BGRA);
+		//memcpy(out, frame.data, frame.total() * frame.elemSize());
 		//this shows the picture but with different colors
 		//this because unity and opencv are using different ways to store colors
 		//i think one uses BGRA and the other RGBA
 		imshow("frame", frame);
 	}
 
-	void __declspec(dllexport) GetLeapDimensions(int dim[]) {
+	/*void __declspec(dllexport) GetLeapDimensions(int dim[]) {
 		configureLogging();
 		LOG(INFO) << "GetLeapDimensions";
 		int width, height;
 		getDimensions(&width, &height);
 		dim[0] = width;
 		dim[1] = height;
-	}
+	}*/
 
-	void __declspec(dllexport) GetLeapImage(unsigned char* out, int index) {
+	/*void __declspec(dllexport) GetLeapImage(unsigned char* out, int index) {
 		int width, height;
 		getDimensions(&width, &height);
 		unsigned char* image = new unsigned char[width * height];
 		getImage(image, index);
 		Mat frame(height, width, CV_8UC1, image);
 		memcpy(out, frame.data, frame.total() * frame.elemSize());
-	}
+	}*/
 
 	void configureLogging() {
 		el::Configurations conf("D:\\Development\\Git\\LeapMotionToolTracking\\TestDLL\\TestDLL\\logging.conf");
