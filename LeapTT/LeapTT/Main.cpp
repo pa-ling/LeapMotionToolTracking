21abd7774b412bec1a6c486b65c0390c8ba346e3
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-02-02 16:09:52 +0100

    Compare marker position with previous positions

--- LeapTT/LeapTT/Main.cpp
+++ LeapTT/LeapTT/Main.cpp
@@ -13,7 +13,7 @@ extern "C" {
 
 	int MASK_RADIUS = 20;
 
-	float lastPositions[2][4] = { { -1, -1, -1, -1 }, { -1, -1, -1, -1 } };
+	float prevPos[2][4] = { { -1, -1, -1, -1 }, { -1, -1, -1, -1 } };
 
 	void __declspec(dllexport) ConvertByteToColor(unsigned char* img8uc1, unsigned char* img8uc3, int width, int height)
 	{
@@ -71,21 +71,48 @@ extern "C" {
 		imshow("Disparity", disp8);
 	}
 
+	int findClosestPointInVector(Point2f point, vector<Point2f> candidates) {
+		float minDist = 0;
+		int nearestPointIndex = 0;
+		for (int i = 0; i < candidates.size(); i++) {
+			Point2f candidate = candidates[i];
+			float euclidianDist = sqrt(pow(point.x - candidate.x, 2) + pow(point.y - candidate.y, 2));
+			if (euclidianDist < minDist) {
+				minDist = euclidianDist;
+				nearestPointIndex = i;
+			}
+		}
+		return nearestPointIndex;
+	}
+
 	void findMarkers(vector<Point2f> center, vector<float> radius, float markerLocations[], int camera)
 	{
 		// Return previous positions if no marker was found
 		if (0 == center.size()) {
 			for (int i = 0; i < 4; i++) {
-				markerLocations[i] = lastPositions[camera][i];
+				markerLocations[i] = prevPos[camera][i];
 			}
-			
 			return;
 		}
 
+		// Match the given point to previous position and return one previous position additionally
 		if (1 == center.size()) {
-			// TODO: Ermittle welcher der beiden punkte gefunden wurde --> welcher der vorherigen punkte ist näher
-			// TODO: Auch radius vergleichen
-			// TODO: Nimm den anderen Punkt von den vorherigen Punkten
+			Point2f candidate = center[0];
+			float euclidianDist0 = sqrt(pow(prevPos[camera][0] - candidate.x, 2) + pow(prevPos[camera][1] - candidate.y, 2));
+			float euclidianDist1 = sqrt(pow(prevPos[camera][2] - candidate.x, 2) + pow(prevPos[camera][3] - candidate.y, 2));
+			if (euclidianDist0 < euclidianDist1) {
+				markerLocations[0] = center[0].x;
+				markerLocations[1] = center[0].y;
+				markerLocations[2] = prevPos[camera][2];
+				markerLocations[3] = prevPos[camera][3];
+			}
+			else {
+				markerLocations[0] = prevPos[camera][0];
+				markerLocations[1] = prevPos[camera][1];
+				markerLocations[2] = center[0].x;
+				markerLocations[3] = center[0].y;
+			}
+			// TODO: Compare radius too
 		}
 
 		if (2 == center.size()) {
@@ -97,9 +124,14 @@ extern "C" {
 		}
 
 		if (2 < center.size()) {
-			// TODO: Ermittle welcher Punkte am nähsten an den beiden vorherigen Punkten sind
-			// TODO: Auch Radius vergleichen
-			// TODO: Nimm diese beiden Punkte
+			int closestPoint0 = findClosestPointInVector(Point2f(prevPos[camera][0], prevPos[camera][1]), center);
+			int closestPoint1 = findClosestPointInVector(Point2f(prevPos[camera][2], prevPos[camera][3]), center);
+			markerLocations[0] = center[closestPoint0].x;
+			markerLocations[1] = center[closestPoint0].y;
+			markerLocations[2] = center[closestPoint1].x;
+			markerLocations[3] = center[closestPoint1].y;
+			// TODO: Compare radius too
+			// TODO: What if both points are the same?
 		}
 	}
 
@@ -129,20 +161,19 @@ extern "C" {
 		for (int i = 0; i < contours.size(); i++) {
 			minEnclosingCircle((Mat)contours[i], center[i], radius[i]);
 			Scalar color = Scalar(0, 0, 255);
-			if (i == 0 || i == 1) {
-				color = Scalar(0, 255, 0);
-			}
 			circle(drawing, center[i], radius[i], color, 1);
 		}
 
-		LOG(INFO) << camera << "(" << lastPositions[camera][0] << lastPositions[camera][1] << lastPositions[camera][2] << lastPositions[camera][3] << ")";
+		LOG(INFO) << camera << "(" << prevPos[camera][0] << ", " << prevPos[camera][1] << ", " << prevPos[camera][2] << ", " << prevPos[camera][3] << ")";
 		LOG(INFO) << camera << "*: " << center;
 
 		findMarkers(center, radius, markerLocations, camera);
+		circle(drawing, Point2f(markerLocations[0], markerLocations[1]), 2, Scalar(0, 255, 0), 2);
+		circle(drawing, Point2f(markerLocations[2], markerLocations[3]), 2, Scalar(0, 255, 0), 2);
 
 		//Save positions for next frame
 		for (int i = 0; i < 4; i++) {
-			lastPositions[camera][i] = markerLocations[i];
+			prevPos[camera][i] = markerLocations[i];
 		}
 
 		imshow("Result", drawing);
