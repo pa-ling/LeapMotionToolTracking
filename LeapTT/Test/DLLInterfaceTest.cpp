bf6b6b812d08a03c6a722d217c49b4af351e6cfc
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-02-03 15:40:55 +0100

    Add test for findClosestPointInVector (not building)

--- LeapTT/Test/DLLInterfaceTest.cpp
+++ LeapTT/Test/DLLInterfaceTest.cpp
@@ -1,8 +1,13 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <opencv2/opencv.hpp>
 #include "..\LeapTT\Main.h"
+#include "..\LeapTT\Util.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace std;
+using namespace cv;
 
 /* NOTE: TEST_METHODS are not allowed to have the same name as any tested method. */
 namespace Test
@@ -12,19 +17,20 @@ namespace Test
 	public:
 		wchar_t message[200];
 
-		TEST_METHOD(TestProcessImageData)
+		TEST_METHOD(TestFindClosestPointInVector)
 		{
-			_swprintf(message, L"Test not implemented yet");
-			Assert::Fail(message);
-		}
+			Point2f point(0, 0);
+			vector<Point2f> candidates;
+			candidates.push_back(Point2f(0, 0));
+			candidates.push_back(Point2f(1, 5));
+			candidates.push_back(Point2f(7, 3));
+			candidates.push_back(Point2f(10, 1));
+			candidates.push_back(Point2f(1, 1));
 
-		TEST_METHOD(TestLeapImages)
-		{
-			//getLeapImages();
-			_swprintf(message, L"Test not implemented yet");
+			int index = Util::findClosestPointInVector(point, candidates);
+			//_swprintf(message, L"" + index);
 			Assert::Fail(message);
-		}
-		
+		}	
 
 	};
 }
