fddcf6005d9f396d513e47164cc4d4a0087433a3
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-02-22 14:11:01 +0100

    All players now use their own camera

---
+++ DLLImport/Assets/Scripts/NetworkCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkCamera : NetworkBehaviour {
+
+	void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            transform.Find("Head/Visor/Camera").gameObject.GetComponent<Camera>().enabled = false;
+            return;
+        }
+    }
+}
