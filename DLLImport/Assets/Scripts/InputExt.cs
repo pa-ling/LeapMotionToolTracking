8fd693acad6ec45521e76491608b23ca96715d79
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-03-01 10:34:04 +0100

    Remove old Sketching Scripts

--- DLLImport/Assets/Scripts/InputExt.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UnityEngine.Extensions {
-    public static class InputExt {
-	    public static bool MouseIsMoved() {
-            return (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) ? true : false;
-        }
-    }
-}
