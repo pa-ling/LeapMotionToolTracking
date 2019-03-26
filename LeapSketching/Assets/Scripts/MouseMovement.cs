121bca93c409929cf88f3801cf4fe7f0e30ef64f
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-03-26 12:54:17 +0100

    Remove ConnectionMonitor, add mouse movement

---
+++ LeapSketching/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour {
+
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 pos = ray.GetPoint(5);
+        transform.position = pos;
+    }
+
+}
